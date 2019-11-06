b101438a18abba94e5783e3ad81b386ec84a6360
Arjun RP <arjun@amagi.com>
2019-11-06 10:57:10 +0530

    initial commit

---
+++ js/tile-player.js
@@ -0,0 +1,50 @@
+
+function getType(url){
+  if (url.indexOf(".m3u8") > 0) {
+    return "application/x-mpegURL"
+  }
+  else if (url.indexOf("rtmp://") >= 0) {
+    return "rtmp/mp4";
+  }
+  else if (url.indexOf(".mp4") > 0) {
+    return "video/mp4"
+  }
+  else {
+    alert("unknown video type: '"+url+"'");
+  }
+}
+
+$(document).ready(()=>{
+
+  $.getJSON("stream-url.json").done((data) => {
+    console.log("links", data.links)
+
+    links = data.links;
+      let players = [];
+
+      for(i=0;i < links.length; i++) {
+        const player_id = `player_${i}`;
+        const link = links[i];
+        $("#players").append("\
+          <div class='player'>\
+            <video autoplay='true' muted='muted' id='"+player_id+"' class='video-js vjs-default-skin' controls preload='auto' width='448' height='252'></video>\
+            <input readonly type='text' value='"+link+"'>\
+          </div>\
+        ");
+
+        const player = videojs(player_id, {
+          autoplay: true,
+          sources:[{
+            src: link,
+            type: getType(link)
+          }]
+        });
+        players.push(player);
+      }
+
+  }).fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    alert( "Request Failed: " + err );
+  });
+
+});
